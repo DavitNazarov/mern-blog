ad0c99468b9fc28cee1db476c677cafeea8639f5
Davit Nazarov <>
2024-11-24 20:03:47 +0400

    some update
    adding slide menu

--- frontend/src/shared/components/Navigation/SideDrawer.jsx
+++ frontend/src/shared/components/Navigation/SideDrawer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
-const SideDrawer = () => {
-  return <div>SideDrawer</div>;
+import "./SideDrawer.css";
+import { CSSTransition } from "react-transition-group";
+const SideDrawer = (prop) => {
+  return (
+    <CSSTransition
+      in={prop.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={prop.onClick}>
+        {prop.children}
+      </aside>
+    </CSSTransition>
+  );
 };
 
 export default SideDrawer;
